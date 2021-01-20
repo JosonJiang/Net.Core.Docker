77d762ab2aa6c478c18be9648d82b061146edbb0
Joson Jiang <87592776@qq.com>
2021-01-20 15:20:22 +0800

    添加项目文件。

---
+++ Net.Web.Docker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net.Web.Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
